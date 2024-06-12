a575d4a1cf61dde8abc9875c2608a2392020ae07
khakestari <mostafanmz@yahoo.com>
2024-06-12 17:33:54 +0330

    project 2

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,28 @@ const char* password = "135792468";
 const char* mqtt_server = "thesoshi.com";
 const int mqtt_port = 2424;
 
-const char* target_topic = "microlab/light";
+const char* clientId = "salam";
+
+const char* target_topic_control = "microlab/light/control";
+const char* target_topic_mode = "microlab/light/mode";
+const char* target_topic_ldr = "microlab/light/ldr";
 
 unsigned long LastTry;
 const int ledPin = 2;  // GPIO pin where the LED is connected
+const int ldrPin = A5; // Analog pin where the LDR is connected
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+bool automaticMode = false;
+int ldrValue = 0;
+
 void setup_wifi();
 void callback(char* topic, byte* message, unsigned int length);
+void setLED(bool state);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(ledPin, OUTPUT);  // Initialize the LED pin as an output
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
@@ -56,25 +65,32 @@ void callback(char* topic, byte* message, unsigned int length) {
   String messageTemp = String((char*)message).substring(0, length);
   Serial.println(messageTemp);
 
-  if (String(topic) == target_topic) {
-    if (messageTemp == "on") {
-      digitalWrite(ledPin, HIGH);  // Turn LED on
-      Serial.println("LED turned ON");
-    } else if (messageTemp == "off") {
-      digitalWrite(ledPin, LOW);  // Turn LED off
-      Serial.println("LED turned OFF");
-    } else {
-      Serial.println("Unknown command");
+  if (String(topic) == target_topic_control) {
+    if (!automaticMode) { // Only handle manual control if not in automatic mode
+      if (messageTemp == "on") {
+        setLED(true);
+      } else if (messageTemp == "off") {
+        setLED(false);
+      }
     }
+  } else if (String(topic) == target_topic_mode) {
+    if (messageTemp == "automatic") {
+      automaticMode = true;
+    } else if (messageTemp == "manual") {
+      automaticMode = false;
+    }
+    Serial.print("Mode set to: ");
+    Serial.println(automaticMode ? "Automatic" : "Manual");
   }
 }
 
 void reconnect() {
   if (millis() - LastTry >= 2000) {
     Serial.print("Attempting MQTT connection...");
-    if (client.connect("ESP32Client")) {
+    if (client.connect(clientId)) {
       Serial.println("connected");
-      client.subscribe(target_topic, 1); // Use QoS 1 for subscription
+      client.subscribe(target_topic_control, 1); // Use QoS 1 for subscription
+      client.subscribe(target_topic_mode, 1);    // Use QoS 1 for subscription
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -84,90 +100,39 @@ void reconnect() {
   }
 }
 
+void setLED(bool state) {
+  digitalWrite(ledPin, state ? HIGH : LOW);
+  Serial.println(state ? "LED turned ON" : "LED turned OFF");
+}
+
 void loop() {
   if (!client.connected()) {
     reconnect();
   } else {
     client.loop();
+
+    if (automaticMode) {
+      // Read LDR value
+      ldrValue = analogRead(ldrPin);
+      // Adjust LED brightness based on LDR value
+      int ledBrightness = map(ldrValue, 0, 4095, 0, 255);
+      analogWrite(ledPin, ledBrightness);
+      Serial.print("LDR value: ");
+      Serial.print(ldrValue);
+      Serial.print(" -> LED brightness: ");
+      Serial.println(ledBrightness);
+
+      // Publish LDR value to MQTT
+      char ldrStr[8];
+      itoa(ldrValue, ldrStr, 10);
+      client.publish(target_topic_ldr, ldrStr, true);
+      delay(1000);
+    } else {
+      // Publish LDR value to MQTT
+      char ldrStr[8];
+      itoa(ldrValue, ldrStr, 10);
+      client.publish(target_topic_ldr, ldrStr, true);
+      delay(1000); // Delay to avoid spamming the loop in manual mode
+    }
   }
 }
-
-// /*------------------------------------------PUB----------------------------------------*/
-
-// #include <WiFi.h>
-// #include <PubSubClient.h>
-
-// #define MSG_BUFFER_SIZE (50)
-
-// const char* ssid = "iphone";
-// const char* password = "135792468";
-
-// const char* mqtt_server = "thesoshi.com";
-// const int mqtt_port = 2424;
-
-// WiFiClient espClient;
-// PubSubClient client(espClient);
-
-// unsigned long lastMsg = 0;
-// char msg[MSG_BUFFER_SIZE];
-// int value = 0;
-
-// void setup() {
-//  Serial.begin(115200);
-//  setup_wifi();
-//  client.setServer(mqtt_server, mqtt_port);
-// }
-
-// void setup_wifi() {
-//  delay(10);
-//  Serial.println();
-//  Serial.print("Connecting to ");
-//  Serial.println(ssid);
-
-//  WiFi.begin(ssid, password);
-
-//  while (WiFi.status() != WL_CONNECTED) {
-//  delay(500);
-//  Serial.print(".");
-//  }
-
-//  Serial.println("");
-//  Serial.println("WiFi connected");
-//  Serial.println("IP address: ");
-//  Serial.println(WiFi.localIP());
-// }
-
-// void reconnect() {
-//  if(millis() - LastTry >= 5000){
-
-//  Serial.print("Attempting MQTT connection...");
-//  if (client.connect("ESP32Client")) {
-//  Serial.println("connected");
-//  client.subscribe(target_topic, 1); // Use QoS 1 for subscription
-//  } else {
-//  Serial.print("failed, rc=");
-//  Serial.print(client.state());
-//  Serial.println(" try again in 5 seconds");
-//  LastTry = millis();
-//  }
-//  }
-// }
-
-// void loop() {
-//  if (!client.connected()) {
-//  reconnect();
-//  }
-//  else{
-//  client.loop();
-
-//  unsigned long now = millis();
-//  if (now - lastMsg > 2000) {
-//  lastMsg = now;
-//  ++value;
-//  snprintf (msg, MSG_BUFFER_SIZE, "hello world #%d", value);
-//  Serial.print("Publish message: ");
-//  Serial.println(msg);
-//  client.publish("outTopic", msg, true);
-//  }
-//  }
-// }
